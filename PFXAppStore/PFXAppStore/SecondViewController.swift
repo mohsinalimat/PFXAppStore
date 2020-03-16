da8fdc6bcf5448ab683c96b956bb607a30435d30
PFXStudio <pfxstudio21@gmail.com>
2020-03-16 10:43:26 +0900

    add proj

---
+++ PFXAppStore/PFXAppStore/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PFXAppStore
+//
+//  Created by PFXStudio on 2020/03/16.
+//  Copyright © 2020 PFXStudio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
